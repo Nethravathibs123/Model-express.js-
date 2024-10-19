92a28f9a5e1536c195bb1a28794aec9cc37d6c04
Nethravathibs123 <nethranethra451@gmail.com>
2024-10-19 20:13:36 +0530

    Modified

--- routes/contactus.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const productsController = require('../controllers/products'); // Make sure the path is correct
-const router = express.Router();
-// Routes for contact
-router.get('/contact', productsController.getContact); // Render Contact Us page
-router.post('/contactus', productsController.postContact); // Handle Contact Us form submission
-router.get('/success', productsController.getSuccess); // Render success message
-module.exports = router;
